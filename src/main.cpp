44476ba987e38e8dfd35fb9fdb1a4e6d362cc379
Manuel Yves Galliker <manuel.galliker@gmx.ch>
2023-06-11 22:01:18 +0200

    Add plot of simulated system

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 
+#include "common/Types.h"
 #include "matplotlibcpp.h"
 #include "ode_solver/EulerForwardSolver.h"
 #include "system_dynamics/MassSpringDamperSystem.h"
 
+namespace plt = matplotlibcpp;
+using namespace lynx_ode;
+
 int main() {
-  lynx_ode::MassSpringDamperSystem system = lynx_ode::MassSpringDamperSystem(2.0, 0.5);
-  lynx_ode::EulerForwardSolver forwardEulerIntegrator = lynx_ode::EulerForwardSolver();
-  lynx_ode::vector_t initialState(2);
+  MassSpringDamperSystem system = MassSpringDamperSystem(2.0, 0.2);
+  EulerForwardSolver forwardEulerIntegrator = EulerForwardSolver();
+  vector_t initialState(2);
   initialState << 1.0, 0.0;
-  auto stateTrajectory = forwardEulerIntegrator.integrate(&system, initialState, 100, 0.1);
-  for (const lynx_ode::vector_t& state : stateTrajectory) {
-    std::cout << state.transpose() << std::endl;
+
+  size_t n_steps = 1000;
+  scalar_t delta_t = 0.01;
+  auto stateTrajectory = forwardEulerIntegrator.integrate(&system, initialState, n_steps, delta_t);
+
+  std::vector<double> x(n_steps + 1), y(n_steps + 1);
+
+  x.emplace_back(0);
+  y.emplace_back(initialState[0]);
+
+  for (int i = 0; i < n_steps; i++) {
+    x.emplace_back(i * delta_t);
+    y.emplace_back(stateTrajectory[i][0]);
   }
+
+  plt::plot(x, y);
+  plt::show();
 
   return 0;
 }
