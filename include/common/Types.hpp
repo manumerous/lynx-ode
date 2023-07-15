3d8c0efa830b356cb0e5d016b12c273c1775138e
Manuel Yves Galliker <manuel.galliker@gmx.ch>
2023-07-15 13:24:37 +0200

    Remove unneded eigen types

--- include/common/Types.hpp
+++ include/common/Types.hpp
@@ -58,36 +58,4 @@ using eigen_scalar_t = Eigen::Matrix<scalar_t, 1, 1>;
 /** Eigen scalar trajectory type. */
 using eigen_scalar_array_t = std::vector<eigen_scalar_t>;
 
-template <typename SCALAR_T>
-using VECTOR_T = Eigen::Matrix<SCALAR_T, -1, 1>;
-template <typename SCALAR_T>
-using VECTOR2_T = Eigen::Matrix<SCALAR_T, 2, 1>;
-template <typename SCALAR_T>
-using VECTOR3_T = Eigen::Matrix<SCALAR_T, 3, 1>;
-template <typename SCALAR_T>
-using VECTOR4_T = Eigen::Matrix<SCALAR_T, 4, 1>;
-template <typename SCALAR_T>
-using VECTOR6_T = Eigen::Matrix<SCALAR_T, 6, 1>;
-template <typename SCALAR_T>
-using VECTOR12_T = Eigen::Matrix<SCALAR_T, 12, 1>;
-template <typename SCALAR_T>
-using MATRIX2_T = Eigen::Matrix<SCALAR_T, 2, 2>;
-template <typename SCALAR_T>
-using MATRIX3_T = Eigen::Matrix<SCALAR_T, 3, 3>;
-template <typename SCALAR_T>
-using MATRIX4_T = Eigen::Matrix<SCALAR_T, 4, 4>;
-template <typename SCALAR_T>
-using MATRIX6_T = Eigen::Matrix<SCALAR_T, 6, 6>;
-
-using vector2_t = VECTOR2_T<scalar_t>;
-using vector3_t = VECTOR3_T<scalar_t>;
-using vector4_t = VECTOR4_T<scalar_t>;
-using vector6_t = VECTOR6_T<scalar_t>;
-using vector12_t = VECTOR12_T<scalar_t>;
-using matrix2_t = MATRIX2_T<scalar_t>;
-using matrix3_t = MATRIX3_T<scalar_t>;
-using matrix4_t = MATRIX4_T<scalar_t>;
-using matrix6_t = MATRIX6_T<scalar_t>;
-using quaternion_t = Eigen::Quaternion<scalar_t>;
-
 }  // namespace lynx_ode
